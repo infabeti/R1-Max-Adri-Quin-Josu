1621dd095dc5a9e6bba0a93417b1e1726cf16feb
maximohernandezga <maximo.hernandezga@elorrieta-errekamari.com>
2021-01-11 22:20:31 +0100

    ampliacion de BBDD

--- RetoFinal/BBDD/euskalmet.sql
+++ RetoFinal/BBDD/euskalmet.sql
@@ -19,9 +19,14 @@ create table ESPACIOS_NATURALES
 (nombre varchar(50) primary key,
 descripcion varchar(2000),
 tipo varchar(50),
-web varchar(200),
-nomMunicipio varchar(50),
-constraint fk_nomMunicipio1 foreign key(nomMunicipio) references MUNICIPIOS(nombre) on update cascade);
+web varchar(200));
+
+create table EXISTE
+(nomMunicipio varchar(50),
+nomEspNat varchar(50),
+constraint fk_nomMunicipio2 foreign key(nomMunicipio) references MUNICIPIOS(nombre) on delete cascade on update cascade,
+constraint fk_nomEspNat foreign key(nomEspNat) references ESPACIOS_NATURALES(nombre) on delete cascade on update cascade,
+constraint pk_existe primary key (nomMunicipio,nomEspNat));
 
 create table ESTACIONES_METEREOLOGICAS
 (nombre varchar(50) primary key,
@@ -29,12 +34,25 @@ direccion varchar(200),
 latitud double,
 longitud double,
 nomMunicipio varchar(50),
-constraint fk_nomMunicipio2 foreign key(nomMunicipio) references MUNICIPIOS(nombre) on delete cascade on update cascade);
+constraint fk_nomMunicipio3 foreign key(nomMunicipio) references MUNICIPIOS(nombre) on delete cascade on update cascade);
+
+create table CALIDAD_AIRE
+(fecha_hora varchar(20) primary key,
+Comgm3 varchar(20), 
+CO8hmgm3 varchar(20), 
+Nogm3 varchar(20), 
+NO2gm3 varchar(20), 
+NOXgm3 varchar(20), 
+PM10gm3 varchar(20), 
+PM25gm3 varchar(20), 
+SO2gm3 varchar(20),
+nomEstMet varchar(50),
+constraint fk_nomEstMet foreign key(nomEstMet) references ESTACIONES_METEREOLOGICAS(nombre) on delete cascade on update cascade);
 
 create table FOTOS
 (id varchar(200) primary key,
 nomMunicipio varchar(50),
-constraint fk_nomMunicipio3 foreign key(nomMunicipio) references MUNICIPIOS(nombre) on delete cascade on update cascade);
+constraint fk_nomMunicipio4 foreign key(nomMunicipio) references MUNICIPIOS(nombre) on delete cascade on update cascade);
 
 create table FOTOS_EUSKALMET
 (id varchar(200) primary key,
