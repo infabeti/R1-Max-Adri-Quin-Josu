4c4ad7515700cbcc1ca94512baeaed018cf8ec47
maximohernandezga <maximo.hernandezga@elorrieta-errekamari.com>
2021-01-12 19:20:06 +0100

    automatizada la descarga de archivos y la conversión a XML

---
+++ RetoFinal/src/main/java/modelo/DescargarJSON.java
@@ -0,0 +1,99 @@
+package modelo;
+
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.X509TrustManager;
+
+public class DescargarJSON {
+
+    /*
+     * Descargará los 4 archivos .json que necesitamos: 
+     * ● JSON (35.02 KB) - Diarios, horarios e índices de calidad del aire 
+     * ● JSON (16.23 KB) - Estaciones de medición 
+     * ● JSON (129.41 KB) - Espacios naturales 
+     * ● JSON (1.03 MB) - Municipios
+     */
+
+    // método para bypass los certificados
+    private static void trustEveryone() {
+	try {
+	    HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+		public boolean verify(String hostname, SSLSession session) {
+		    return true;
+		}
+	    });
+	    SSLContext context = SSLContext.getInstance("TLS");
+	    context.init(null, new X509TrustManager[] { new X509TrustManager() {
+		public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+		}
+
+		public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+		}
+
+		public X509Certificate[] getAcceptedIssuers() {
+		    return new X509Certificate[0];
+		}
+	    } }, new SecureRandom());
+	    HttpsURLConnection.setDefaultSSLSocketFactory(context.getSocketFactory());
+	} catch (Exception e) { // nunca devería ocurrir
+	    e.printStackTrace();
+	}
+    }
+
+    public static void main(String[] args) {
+	String[] direcciones = {
+		"https://opendata.euskadi.eus/contenidos/ds_informes_estudios/calidad_aire_2020/es_def/adjuntos/index.json",
+		"https://opendata.euskadi.eus/contenidos/ds_informes_estudios/calidad_aire_2020/es_def/adjuntos/estaciones.json",
+		"https://opendata.euskadi.eus/contenidos/ds_recursos_turisticos/playas_de_euskadi/opendata/espacios-naturales.json",
+		"https://opendata.euskadi.eus/contenidos/ds_registros/registro_entidades_locales/opendata/entidades.json" };
+	String[] archivos = { "index.json", "estaciones.json", "espacios-naturales.json", "municipios.json" };
+
+	try {
+	    trustEveryone(); // Para evitar el error de los certificados
+	    for (int num = 1; num <= 4; num++) {
+
+		// Urls con los enlaces
+		URL url = new URL(direcciones[num - 1]);
+
+		// Cojo los datos de las entidades municipales, que están más limpios
+
+		// establecemos conexion
+		URLConnection urlCon = url.openConnection();
+
+		// Sacamos por pantalla el tipo de fichero
+		System.out.println(urlCon.getContentType());
+
+		// Se obtiene el inputStream del archivo web y se abre el fichero
+		// local.
+		InputStream is = urlCon.getInputStream();
+		FileOutputStream fos = new FileOutputStream("./archivos/" + archivos[num - 1]);
+
+		// Lectura del archivo de la web y escritura en fichero local
+		byte[] array = new byte[1000]; // buffer temporal de lectura.
+		int leido = is.read(array);
+		while (leido > 0) {
+		    fos.write(array, 0, leido);
+		    leido = is.read(array);
+		}
+
+		// cierre de conexion y fichero.
+		is.close();
+		fos.close();
+	    }
+	} catch (Exception e) {
+	    e.printStackTrace();
+	}
+    }
+
+}
--- RetoFinal/src/main/java/modelo/JSONtoXML.java
+++ RetoFinal/src/main/java/modelo/JSONtoXML.java
@@ -15,58 +15,70 @@ import org.json.JSONException; //http://bit.ly/12O4D2w
 import org.json.JSONObject;
 
 /*
- * Convierte archivos de tipo JSON a XML en cuatro pasos:
+ * Convierte archivos de tipo JSON a XML en cinco pasos:
  * 1) Lee el JSON y lo pasa a un String
- * 2) Renombrar etiquetas duplicadas en el string y borrar las vacias
- * 	(Nota: está particularizado para el archivo espacios-naturales.json. Mejora --> Generalizar solución)
- * 3) Utiliza un objeto json de la libreria org.json para convertirlo a formato XML y guardarlo en un string
- * 4) Escribe el archivo de salida 
+ * 2) Prepara el archivo, quitando cabecera, añadiendo contenedor y arreglando el final
+ * 3) Renombrar etiquetas duplicadas en el string y borrar las vacias
+ * 4) Utiliza un objeto json de la libreria org.json para convertirlo a formato XML y guardarlo en un string
+ * 5) Escribe el archivo de salida 
  */
 
 public class JSONtoXML {
-    private static String direccionArchivoEntrada = "./archivos/espacios-naturales.json";
-    private static String direccionArchivoSalida = "./archivos/espacios-naturales.xml";
+    static String[] archivos = { "index", "estaciones", "espacios-naturales", "municipios" };
+    static String[] contenedores = { "index", "estación", "espacio-natural", "municipio" };
 
     public static void main(String[] args) throws FileNotFoundException, IOException, JSONException {
-	// Lee el archivo JSON
-	long tiempoInicial = System.currentTimeMillis();
-	String json = leerArchivo(direccionArchivoEntrada); // Lee el archivo
-	long tiempoFinal = System.currentTimeMillis();
-	long duracion = tiempoFinal - tiempoInicial;
-	System.out.println("Duración de la lectura del archivo: " + duracion); // Estadística
+	for (int num = 0; num < 4; num++) {
+	    String direccionArchivoEntrada = "./archivos//" + archivos[num] + ".json";
+	    String direccionArchivoSalida = "./archivos//" + archivos[num] + ".xml";
+	    // Lee el archivo JSON
+	    long tiempoInicial = System.currentTimeMillis();
+	    String jsonOrigen = leerArchivo(direccionArchivoEntrada); // Lee el archivo
+	    long tiempoFinal = System.currentTimeMillis();
+	    long duracion = tiempoFinal - tiempoInicial;
+	    System.out.println("Duración de la lectura del archivo: " + duracion + " mlseg"); // Estadística
 
-	// Eliminar etiquetas duplicadas y borrar las vacias
-	tiempoInicial = System.currentTimeMillis();
-	String jsonCorregido = eliminarEtiquetasDuplicadas(json);
-	tiempoFinal = System.currentTimeMillis();
-	duracion = tiempoFinal - tiempoInicial;
-	System.out.println("Duración de renombrar etiquetas duplicadas y borrar vacias: " + duracion); // Estadística
+	    tiempoInicial = System.currentTimeMillis();
+	    String jsonPreparado = prepararArchivo(jsonOrigen, contenedores[num]);
+	    tiempoFinal = System.currentTimeMillis();
+	    duracion = tiempoFinal - tiempoInicial;
+	    System.out.println(
+		    "Duración de preparar el archivo quitando cabecera y borrando final: " + duracion + " mlseg"); // Estadística
 
-	// Convierte JSON a XML
-	tiempoInicial = System.currentTimeMillis();
-	String xml = convertir(jsonCorregido, "root");// Establezco el nombre del tag raiz del XML
-	tiempoFinal = System.currentTimeMillis();
-	duracion = tiempoFinal - tiempoInicial;
-	System.out.println("Duración de la conversión: " + duracion); // Estadística
-	
-	// Escribe el archivo XML
-	tiempoInicial = System.currentTimeMillis();
-	escribirArchivo(direccionArchivoSalida, xml);
-	tiempoFinal = System.currentTimeMillis();
-	duracion = tiempoFinal - tiempoInicial;
-	System.out.println("Duración de escritura de archivo: " + duracion); // Estadística
+	    // Eliminar etiquetas duplicadas y borrar las vacias
+	    tiempoInicial = System.currentTimeMillis();
+	    String jsonCorregido = renombrarEtiquetasDuplicadas(jsonPreparado);
+	    tiempoFinal = System.currentTimeMillis();
+	    duracion = tiempoFinal - tiempoInicial;
+	    System.out.println("Duración de renombrar etiquetas duplicadas y borrar vacias: " + duracion + " mlseg"); // Estadística
+
+	    // Convierte JSON a XML
+	    tiempoInicial = System.currentTimeMillis();
+	    String xml = convertir(jsonCorregido, "root");// Establezco el nombre del tag raiz del XML
+	    tiempoFinal = System.currentTimeMillis();
+	    duracion = tiempoFinal - tiempoInicial;
+	    System.out.println("Duración de la conversión: " + duracion + " mlseg"); // Estadística
+
+	    // Escribe el archivo XML
+	    tiempoInicial = System.currentTimeMillis();
+	    escribirArchivo(direccionArchivoSalida, xml);
+	    tiempoFinal = System.currentTimeMillis();
+	    duracion = tiempoFinal - tiempoInicial;
+	    System.out.println("Duración de escritura de archivo: " + duracion + " mlseg"); // Estadística
+	}
     }
 
     public static String leerArchivo(String rutaArchivo) throws FileNotFoundException, IOException {
 	StringBuilder acumuladoCadena = new StringBuilder();
-	InputStream in = new FileInputStream(direccionArchivoEntrada);
-	Charset encoding = Charset.defaultCharset();
+	InputStream in = new FileInputStream(rutaArchivo);
+//	Charset encoding = Charset.defaultCharset();
+	Charset encoding = Charset.forName("UTF-8"); // Codifico el archivo con UTF-8
 
 	Reader lector = new InputStreamReader(in, encoding);
 
 	int r = 0;
 	while ((r = lector.read()) != -1) {// OJO! uso read() mejor que readLine()
-					   // porque puedo procesar archivos más largos con read()
+	    // porque puedo procesar archivos más largos con read()
 	    char ch = (char) r;
 	    acumuladoCadena.append(ch);
 	}
@@ -77,7 +89,34 @@ public class JSONtoXML {
 	return acumuladoCadena.toString();
     }
 
-    public static String eliminarEtiquetasDuplicadas(String archivo) {
+    public static String prepararArchivo(String archivo, String contenedor) {
+	String archivoSinCabecera = "";
+	String archivoSinFinal = "";
+	String archivoFinal = "";
+	boolean encontrado = false;
+	String[] campos;
+
+	for (int i = 0; i < archivo.length(); i++) {
+	    if (archivo.charAt(i) == '{' && !encontrado) {
+		archivoSinCabecera = archivo.substring(i);
+		encontrado = true;
+	    }
+	}
+	campos = archivoSinCabecera.split("}");
+	archivoSinFinal += campos[0];
+	for (int i = 1; i < campos.length - 1; i++) {
+	    archivoSinFinal += "\n}" + campos[i];
+	}
+
+	if (archivo.charAt(0) == '{')
+	    archivoFinal = archivoSinFinal + "}\n]\n}";
+	else
+	    archivoFinal = "{\n" + "\"" + contenedor + "\"" + ": [ \n" + archivoSinFinal + "\n}\n]\n}";
+
+	return archivoFinal;
+    }
+
+    public static String renombrarEtiquetasDuplicadas(String archivo) {
 	String archivoCorregido = "";
 	String archivoFinal = "";
 	int contador1 = 0;
@@ -93,10 +132,12 @@ public class JSONtoXML {
 		    archivoCorregido += "\"turismDescription" + contador1 + "\"" + " : ";
 		    contador1++;
 		} else if (lineas[j].toString().contains("\"address\"") && contador2 > 0) {
-		    archivoCorregido += lineas[j].substring(0, lineas[j].length() - 9) + "\"address" + contador2 + "\"" + " : ";
+		    archivoCorregido += lineas[j].substring(0, lineas[j].length() - 9) + "\"address" + contador2 + "\""
+			    + " : ";
 		    contador2++;
 		} else if (lineas[j].toString().contains("\"phone\"") && contador3 > 0) {
-		    archivoCorregido += lineas[j].substring(0, lineas[j].length() - 7) + "\"phone" + contador3 + "\"" + " : ";
+		    archivoCorregido += lineas[j].substring(0, lineas[j].length() - 7) + "\"phone" + contador3 + "\""
+			    + " : ";
 		    contador3++;
 		} else if (lineas[j].toString().contains("\"turismDescription\"") && contador1 == 0) {
 		    archivoCorregido += lineas[j] + " : " + lineas[j + 1].substring(0, lineas[j + 1].length() - 19);
@@ -109,33 +150,37 @@ public class JSONtoXML {
 		    contador3++;
 		} else {
 		    archivoCorregido += lineas[j];
-		    if (lineas[j].charAt(lineas[j].length()-1) != (char) 10)
+		    if (lineas[j].charAt(lineas[j].length() - 1) != (char) 10)
 			archivoCorregido += " : ";
 		}
 	    }
+	    archivoCorregido = archivoCorregido.substring(0, archivoCorregido.length() - 2);
+
 	    lineas = null;
 	    contador1 = 0;
 	    contador2 = 0;
 	    contador3 = 0;
 	    archivoCorregido += "}";
 	}
-	
-	//Eliminar etiquetas vacias
+	archivoCorregido = archivoCorregido.substring(0, archivoCorregido.length() - 1);
+	archivoCorregido += "]\n}";
+	// Eliminar etiquetas vacias
 	lineas = archivoCorregido.split(",");
 	for (int i = 0; i < lineas.length; i++) {
 	    if (!lineas[i].toString().contains("\"\"")) {
 		archivoFinal += lineas[i] + ",";
 	    }
 	}
+	archivoFinal = archivoFinal.substring(0, archivoFinal.length() - 1); // Le quita la última ","
 
 	return archivoFinal;
     }
 
     public static String convertir(String json, String raiz) throws JSONException {
 	JSONObject objetoJson = new JSONObject(json);
-	String xml = "<?xml version=\"1.0\" encoding=\"ISO-8859-15\"?><" + raiz + ">"
-		+ org.json.XML.toString(objetoJson) + "</" + raiz + ">";
-	
+	String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><" + raiz + ">" + org.json.XML.toString(objetoJson)
+		+ "</" + raiz + ">";
+
 	return xml;
     }
 
