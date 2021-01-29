6713242854b3aa9f99b43b0af8d7115edb420800
maximohernandezga <maximo.hernandezga@elorrieta-errekamari.com>
2021-01-29 10:05:35 +0100

    si los archivos ya existen no hace descarga

--- RetoFinal/src/main/java/Iniciadores/IniciarServer.java
+++ RetoFinal/src/main/java/Iniciadores/IniciarServer.java
@@ -1,5 +1,6 @@
 package Iniciadores;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
@@ -15,23 +16,44 @@ import modelo.Servidor;
 
 public class IniciarServer {
 
-	public static void main(String[] args) throws FileNotFoundException, JSONException, IOException{
-		// Arranca el servidor
-		Servidor servidor = new Servidor();
-		servidor.start();
-		
-		DescargarJSON descargarJSON = new DescargarJSON();
-		JSONtoXML jsonToXml = new JSONtoXML();
-		InsertarDatosGeograficos insertarDatosGeograficos = new InsertarDatosGeograficos();
-		DescargarDatosMetereologicos descargarDatosMetereologicos = new DescargarDatosMetereologicos();
-		InsertarDatosAtmosfericos insertarDatosAtmosfericos = new InsertarDatosAtmosfericos();
-		InsertarRelacionExiste insertarRelacionExiste = new InsertarRelacionExiste();
+    public static void main(String[] args) throws FileNotFoundException, JSONException, IOException {
+	boolean existen = false;
+	// Arranca el servidor
+	Servidor servidor = new Servidor();
+	servidor.start();
 
-		descargarJSON.DescargarJSON();
-		jsonToXml.JSONtoXML();
-		insertarDatosGeograficos.InsertarDatosGeograficos();
-		descargarDatosMetereologicos.DescargarDatosMetereologicos();
-		insertarDatosAtmosfericos.InsertarDatosAtmosfericos();
-		insertarRelacionExiste.InsertarRelacionExiste();
+	File archivoMunicipios = new File("./archivos/municipios.json");
+	File archivoIndex = new File("./archivos/index.json");
+	File archivoEstaciones = new File("./archivos/estaciones.json");
+	File archivoEspNaturales = new File("./archivos/espacios-naturales.json");
+	
+	if (!archivoIndex.exists()) {
+	    existen = true;
 	}
+	if (!archivoMunicipios.exists()) {
+	    existen = true;
+	}
+	if (!archivoEstaciones.exists()) {
+	    existen = true;
+	}
+	if (!archivoEspNaturales.exists()) {
+	    existen = true;
+	}
+
+	if (!existen) {
+	    DescargarJSON descargarJSON = new DescargarJSON();
+	    JSONtoXML jsonToXml = new JSONtoXML();
+	    InsertarDatosGeograficos insertarDatosGeograficos = new InsertarDatosGeograficos();
+	    DescargarDatosMetereologicos descargarDatosMetereologicos = new DescargarDatosMetereologicos();
+	    InsertarDatosAtmosfericos insertarDatosAtmosfericos = new InsertarDatosAtmosfericos();
+	    InsertarRelacionExiste insertarRelacionExiste = new InsertarRelacionExiste();
+
+	    descargarJSON.DescargarJSON();
+	    jsonToXml.JSONtoXML();
+	    insertarDatosGeograficos.InsertarDatosGeograficos();
+	    descargarDatosMetereologicos.DescargarDatosMetereologicos();
+	    insertarDatosAtmosfericos.InsertarDatosAtmosfericos();
+	    insertarRelacionExiste.InsertarRelacionExiste();
+	}
+    }
 }
